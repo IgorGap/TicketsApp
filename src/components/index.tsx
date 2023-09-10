edb478ca2c477e3136e615f8df94d2aad5892367
IgorGap <ig.gaponenko@gmail.com>
2023-09-10 14:50:22 -0300

    start codding

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+export const Home: React.FC = () => {
+  return (
+    <>
+      <div className={styles.wrapp}>1234</div>
+    </>
+  )
+}
